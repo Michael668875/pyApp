c96b6197b69fcc261f160b9709e929c75687d5d2
Michael668875 <159018843+Michael668875@users.noreply.github.com>
2025-07-22 02:17:09 +1000

    another commit

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 #from this video: https://www.youtube.com/watch?v=f_9NBdSAo-g
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
+
+#App Settings
+app = QApplication([])
+main_window = QWidget()
+main_window.setWindowTitle("My First App")
+main_window.resize(300,200)
+
+#Create all Object/Widgets below here
+
+main_window.show()
+
+app.exec_()
