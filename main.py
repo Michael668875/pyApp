bfe8114fdec23bf1764889c3c10a1ff476f06267
Michael668875 <159018843+Michael668875@users.noreply.github.com>
2025-07-24 19:21:06 +1000

    finished first app

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
 
+from random import choice
+
 # Main App Objects and Settings
 app = QApplication([])
 main_window = QWidget()
@@ -20,6 +22,8 @@ text3 = QLabel("?")
 button1 = QPushButton("Click Me")
 button2 = QPushButton("Click Me")
 button3 = QPushButton("Click Me")
+
+my_words = ["hello", "goodbye", "test", "app", "python", "pyqt", "vietnam", "asia"]
 
 #All Design here
 master_layout = QVBoxLayout()
@@ -44,7 +48,23 @@ master_layout.addLayout(row3)
 
 main_window.setLayout(master_layout)
 
+# Create functions
+def random_word1():
+    word = choice(my_words)
+    text1.setText(word)
+
+def random_word2():
+    word = choice(my_words)
+    text2.setText(word)
+
+def random_word3():
+    word = choice(my_words)
+    text3.setText(word)
+
 # Events
+button1.clicked.connect(random_word1)
+button2.clicked.connect(random_word2)
+button3.clicked.connect(random_word3)
 
 # Show/run our App
 main_window.show()
