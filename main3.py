9318336507f0b034de6daf08aaad2cba3805ea8b
Michael668875 <159018843+Michael668875@users.noreply.github.com>
2025-08-07 22:54:18 +1000

    started photo app - project 3

---
+++ main3.py
@@ -0,0 +1,8 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QListWidget, QComboBox, QHBoxLayout, QVBoxLayout
+from PyQt5.QtCore import Qt
+
+app = QApplication([])
+main_window = QWidget()
+main_window.setWindowTitle("PhotoQT")
+main_window.resize(900, 700)
+
